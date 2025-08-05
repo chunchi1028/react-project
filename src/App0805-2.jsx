bb9382d6c648c6a959ef51efab8bb19a3389fba6
chunchi1028 <wsjennt1028@gmail.com>
2025-08-05 17:33:44 +0800

    20250805

---
+++ src/App0805-2.jsx
@@ -0,0 +1,64 @@
+import './css/App0805-02.css'
+function App() {
+    return (
+        <>
+            <div className="wrap">
+                <h1>HOT SPOT｜大溪老街藏和風秘境，「星巴克大溪公園門市」開幕！日式老屋結合在地美學，咖啡控新據點</h1>
+                <img src="https://today-obs.line-scdn.net/0hAAVzpELlHm5rAwAa_P9hOVNVEh9YZQRnSWJUX0kHQgpAL1FrX2xNDRtXR0JPO1owS2RYDUdUQ1hBZF0wUQ/w1200" />
+                <p>
+                    走進桃園大溪老城區，除了古意盎然的街道與悠悠人文，如今又多了一處靜謐的日式咖啡空間。星巴克全台首間以日式和風建築打造的門市正式落腳大溪，選址於普濟路旁綠意圍繞的大溪公園，於5月1日歡慶開幕。從建築語彙到商品設計，揉合在地文化、職人美感與日常細節，讓喝咖啡這件事不再只是片刻休息，而是一場充滿美學體驗的生活儀式。
+                </p>
+                <img src="https://today-obs.line-scdn.net/0hAAVzpELlHm5rAwAa_P9hOVNVEh9YZQRnSWJUX0kHQgpAL1FrX2xNDRtXR0JPO1owS2RYDUdUQ1hBZF0wUQ/w1200" />
+                <p>「星巴克大溪公園門市」亮點#01▼從屋瓦到傢俱都講究，大溪老街裡的日式美學場域</p>
+                <p>
+
+                    這棟隱身於大溪老街內的門市建築，承襲日式傳統斜屋頂形式，設計上特別保留木結構原貌，並打開屋頂結構，導入自然光與通風。選用中性沉穩的色調與原木家具，搭配懷舊道具與細節質感，勾勒出一種穿越時光的沉靜氛圍。邀請每一位來訪者，在這裡慢慢享受時間推移的節奏，如同旅人駐足森林深處，與咖啡香共度片刻寧靜。
+                </p>
+                <figure>
+                    <img src="https://today-obs.line-scdn.net/0hrqXr0ZwCLV5lCzMq8vNSCV1dIS9WbTdXR2swPRMKIzpNJ20MWzl-PUkIcnIYOGoMRWsxbEANdGkYM2tdCw/w1200" />
+                    <img src="https://today-obs.line-scdn.net/0hhgYuikOoN0hkDCk88_ZIH1xaOzlXai1BRmJ6KUlfayscIHkZD2pkK0lfb2QZPSQWRDovKkgKbXhMaSIeWw/w1200" />
+                    <img src="https://today-obs.line-scdn.net/0h7c_dIzu1aExiHXY49eUXG1pLZD1Re3JFQHgjLUZNZXlMMSoZWSk7L0YUZmAcKHhNQiwhI04eY3hLeSwSVg/w1200" />
+                    <img src="https://today-obs.line-scdn.net/0hWWvza3yKCHhLAxYM3Pt3L3NVBAl4ZRJxaTYQSWlRUR0zL0d6JTVbGz4CUlRvMEkuazUTSTsAVxtuZkZ-fw/w1200" />
+                    <img src="https://today-obs.line-scdn.net/0hVfxtYAirCUlFDRc90vZ2Hn1bBTh2axNAZzlHKmEKXnE7IRoeLDlaKmILAmU4NBpIZW1Ae2RfU35vbx5LcA/w1200" />
+                    <img src="https://today-obs.line-scdn.net/0hXkDGdjVNB0R5Phkw7sZ4E0FoCzVKWB1NWw9LdQk-WyBXEkEWQFFUJ1lqXGgEDkBGWVxPcQluXSZXCkEbQQ/w1200" />
+                    <figcaption>圖片來源...</figcaption>
+                </figure>
+                <p>「星巴克大溪公園門市」亮點#02▼「麥芽豆奶福吉茶那堤」限定登場</p>
+                <p>
+
+                    為迎接大溪公園門市開幕，星巴克首度推出限定飲品「麥芽豆奶福吉茶那堤」，以大溪在地茶文化為靈感，選用福吉茶為基底，結合圓潤香滑的豆奶與麥芽奶油泡沫，展現濃郁卻溫柔的口感。杯面撒上福吉茶粉，彷彿映照著大溪橋的優雅曲線與山水景致，即日起至5月31日，購買大杯享優惠價135元。
+                </p>
+                <img src="https://today-obs.line-scdn.net/0hvMD_JY4bKWJ5JjcW7tZWNUFwJRNKQDNrW0MyUA4kJAUBCjkxR0B6AQ4ndE4HEDkyWUZkBQl1dwVWFW9jQA/w1200" />
+                <p>「星巴克大溪公園門市」亮點#03▼豆干變時尚？大溪限定商品融合在地靈魂</p>
+                <p>
+
+                    星巴克同步推出一系列以大溪元素為主題的限定商品。設計靈感來自當地特產豆干的「棕女神尼龍肩背保冷袋」，運用大地棕色與絎縫線條描繪豆干外型，並巧妙縫上造型裝飾片，展現幽默趣味與地方記憶。還有印製大溪老街、日式屋瓦意象的「大溪風情馬克杯」與「大溪風情隨行卡」，件件都是設計控與旅人值得珍藏的日常風景。
+                </p>
+                <figure>
+                    <img src="https://today-obs.line-scdn.net/0hpsFSh55sL1x7KjEo7NFQC0N8Iy1ITDVVWUhpbgooI2xSBmoLEBh8Pwp-c3AFTWhaW0lmaQt4IjsBTmwPQQ/w1200" />
+                    <img src="https://today-obs.line-scdn.net/0hIfuRPCx9FklIKwg939lpHnB9Gjh7TQxAah8Lem5_H3hhB1QZchpFKmV7T2VtS1MXaBkLe2x_H3ljGlUXdA/w1200" />
+                    <img src="https://today-obs.line-scdn.net/0hyqFBwyzgJl57Ozgq7MFZCUNtKi9IXTxXWVtqbFw_cG0DFzNYFw91PVdrf3IFXGRfW1s7bQs9fD0FXmMOQQ/w1200" />
+                </figure>
+
+                <p>
+                    為歡慶大溪公園門市開幕，星巴克自5月1日起至31日推出多項限定活動，包括單筆消費滿800元可獲旅行收納包，滿1800元贈雙層野餐盒，購買指定商品組合則送保暖毯。活動期間打卡「Meet Me @Starbucks-大溪公園」，購買兩杯相同飲品可享第二杯半價。星禮程會員於5月10日前消費滿350元，還可獲好友分享優惠券。以上活動限大溪公園門市，贈品數量有限，送完為止。
+                </p>
+                <ul >
+                    <li>星巴克大溪公園門市</li><br />
+                    <li>地址：桃園市大溪區普濟路37之2號</li><br />
+                    <li>電話：03-3886973</li><br />
+                    <li>營業時間：平日07:00至19:00、假日07:00至20:00</li><br />
+                </ul>
+
+
+                <h3>【推薦你看】</h3>
+                <ul>
+                    <li><a href="https://www.vogue.com.tw/article/%E5%A2%A8%E8%B3%9E-%E9%90%B5%E6%9D%BF%E7%87%92">HOT SPOT｜母親節大餐新選擇！「墨賞」新鐵板料理古亭店開幕，以法式酒窖美學與無國界鐵板料理寵愛媽咪</a></li>
+
+                    <li><a href="https://www.vogue.com.tw/article/kaws-holiday-%E6%9B%BC%E8%B0%B7">Vogue去哪玩｜泰國最潮新景點！「KAWS:HOLIDAY」曼谷站5月登場，曼谷皇家田廣場變身巨型露天藝術場馬上成為VOGUE LINE好友，獨家超神準星座運勢搶先看！</a></li>
+                </ul>
+            </div>
+        </>
+    )
+}
+export default App;
